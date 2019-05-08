8ef61b077665041963efc3453e29224b12d0f5de
skosiu147 <skosiu147@users.noreply.github.com>
2019-05-08 16:08:20 +0200

    game works

--- tictac.js
+++ tictac.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         board: [],
+        winner: '',
         x: true,
         o: false,
     },
@@ -18,6 +19,46 @@ var app = new Vue({
                 this.x = true;
                 this.o = false;
                 }
+
+            this.findWinner();
+        },
+        findWinner(){
+            //find winner x
+            if(this.board[0] == 'x' && this.board[1] == 'x' && this.board[2] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[3] == 'x' && this.board[4] == 'x' && this.board[5] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[6] == 'x' && this.board[7] == 'x' && this.board[8] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[0] == 'x' && this.board[3] == 'x' && this.board[6] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[1] == 'x' && this.board[4] == 'x' && this.board[7] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[2] == 'x' && this.board[5] == 'x' && this.board[8] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[0] == 'x' && this.board[4] == 'x' && this.board[8] == 'x'){
+                this.winner = 'x';
+            }else if(this.board[2] == 'x' && this.board[4] == 'x' && this.board[6] == 'x'){
+                this.winner = 'x';
+            }
+            // find winner o
+            else if(this.board[0] == 'o' && this.board[1] == 'o' && this.board[2] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[3] == 'o' && this.board[4] == 'o' && this.board[5] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[6] == 'o' && this.board[7] == 'o' && this.board[8] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[0] == 'o' && this.board[3] == 'o' && this.board[6] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[1] == 'o' && this.board[4] == 'o' && this.board[7] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[2] == 'o' && this.board[5] == 'o' && this.board[8] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[0] == 'o' && this.board[4] == 'o' && this.board[8] == 'o'){
+                this.winner = 'o';
+            }else if(this.board[2] == 'o' && this.board[4] == 'o' && this.board[6] == 'o'){
+                this.winner = 'o';
+            }
         },
     },
 })
